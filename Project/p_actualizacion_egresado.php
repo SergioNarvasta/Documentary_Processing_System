e700f80862a48df4f8edb4de73e7b21b76b814b5
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-15 17:02:20 -0500

    Update Process

--- Project/p_actualizacion_egresado.php
+++ Project/p_actualizacion_egresado.php
@@ -1,8 +1,10 @@
 <?php 
+
 include("utils/conexion.php");
 include('utils/auth.php');
 $codigo=$_SESSION['usuario'];
 $tipo=$_SESSION['tipo'];
+
 //recepcionar los datos del egresado
 $dni=$_POST['dni'];
 $facultad=$_POST['lstfacultad'];
@@ -12,10 +14,12 @@ $nombres=$_POST['nombres'];
 $direccion=$_POST['direccion'];
 $email=$_POST['email'];
 $celular=$_POST['celular'];
+
 //tratar informacion
 $apellidos=strtoupper(trim($apellidos));
 $nombres=strtoupper(trim($nombres));
 $direccion=strtoupper(trim($direccion));
+
 //actualizar datos
 $sql="update egresado set dni='$dni',idescuela=$escuela,apellidos='$apellidos',nombres='$nombres',direccion='$direccion', email='$email', celular='$celular',estado=1 where idusuario=$codigo ";
 mysqli_query($cn,$sql);
--- Project/p_actualizacion_institucion.php
+++ Project/p_actualizacion_institucion.php
@@ -1,17 +1,21 @@
 <?php 
+
 include("utils/conexion.php");
 include('utils/auth.php');
 $codigo=$_SESSION['usuario'];
 $tipo=$_SESSION['tipo'];
+
 //recepcionar los datos del egresado
 $ruc=$_POST['ruc'];
 $razonsocial=$_POST['razonsocial'];
 $direccion=$_POST['direccion'];
 $email=$_POST['email'];
 $telefono=$_POST['telefono'];
+
 //tratar informacion
 $razonsocial=strtoupper(trim($razonsocial));
 $direccion=strtoupper(trim($direccion));
+
 //actualizar datos
 $sql="update institucion set ruc='$ruc', razonsocial='$razonsocial',direccion='$direccion', email='$email', telefono='$telefono',estado=1 where idusuario=$codigo ";
 mysqli_query($cn,$sql);
